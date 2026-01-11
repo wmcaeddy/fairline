dda431d4170251325fcabf1c6452354666bf0127
wmcaeddy <wmcaeddy@yahoo.com.hk>
2026-01-12 00:14:04 +0800

    feat(ui): Update hero items with ProNew assets and titles

--- components/hero.php
+++ components/hero.php
@@ -1,10 +1,67 @@
 <div class="banner scroll-fade scroll-view" data-bottom-top="transform:translate3d(0px, -50%, 0px)" data-top-bottom="transform:translate3d(0px, 50%, 0px)">
   <div class="slider-cover">
+    <!-- Slide 1: 軟體反盜版 -->
     <div class="item on">
-      <a href="javascript:void(0);" title="">
+      <a href="javascript:void(0);" title="軟體反盜版">
+        <div class="bgimg">
+          <div class="img hidden-portrait" style="background-image: url('assets/pronew/main_1.jpg');"></div>
+          <div class="img visible-portrait" style="background-image: url('assets/pronew/main_1.jpg');"></div>
+        </div>
+        <div class="inner">
+          <p class="text1">軟體反盜版</p>
+          <p class="text2">Software Anti-Piracy</p>
+        </div>
+      </a>
+    </div>
+    <!-- Slide 2: 軟體保護策略 -->
+    <div class="item">
+      <a href="javascript:void(0);" title="軟體保護策略">
+        <div class="bgimg">
+          <div class="img hidden-portrait" style="background-image: url('assets/pronew/main_2.jpg');"></div>
+          <div class="img visible-portrait" style="background-image: url('assets/pronew/main_2.jpg');"></div>
+        </div>
+        <div class="inner">
+          <p class="text1">軟體保護策略</p>
+          <p class="text2">Software Protection Strategy</p>
+        </div>
+      </a>
+    </div>
+    <!-- Slide 3: 數位身分認證 -->
+    <div class="item">
+      <a href="javascript:void(0);" title="數位身分認證">
+        <div class="bgimg">
+          <div class="img hidden-portrait" style="background-image: url('assets/pronew/main_3.jpg');"></div>
+          <div class="img visible-portrait" style="background-image: url('assets/pronew/main_3.jpg');"></div>
+        </div>
+        <div class="inner">
+          <p class="text1">數位身分認證</p>
+          <p class="text2">Digital Identity Authentication</p>
+        </div>
+      </a>
+    </div>
+    <!-- Slide 4: 線上服務認證 -->
+    <div class="item">
+      <a href="javascript:void(0);" title="線上服務認證">
+        <div class="bgimg">
+          <div class="img hidden-portrait" style="background-image: url('assets/pronew/main_4.jpg');"></div>
+          <div class="img visible-portrait" style="background-image: url('assets/pronew/main_4.jpg');"></div>
+        </div>
+        <div class="inner">
+          <p class="text1">線上服務認證</p>
+          <p class="text2">Online Service Certification</p>
+        </div>
+      </a>
+    </div>
+    <!-- Slide 5: 數位資料保密 -->
+    <div class="item">
+      <a href="javascript:void(0);" title="數位資料保密">
         <div class="bgimg">
           <div class="img hidden-portrait" style="background-image: url('assets/pronew/main_5.jpg');"></div>
-          <div class="img visible-portrait" style="background-image: url('assets/pronew/main_1.jpg');"></div>
+          <div class="img visible-portrait" style="background-image: url('assets/pronew/main_5.jpg');"></div>
+        </div>
+        <div class="inner">
+          <p class="text1">數位資料保密</p>
+          <p class="text2">Digital Data Confidentiality</p>
         </div>
       </a>
     </div>
